0058cc29d96f1e8a2cd84234375249d14236aa0a
jmscoding100 <joshua.z.mcgee@gmail.com>
2024-12-12 11:51:48 -0600

    dice-2

--- js/apps.js
+++ js/apps.js
@@ -45,8 +45,8 @@ class Game {
         this.winStreakDisplay = document.getElementById('winStreakDisplay')
         this.lossStreakDisplay = document.getElementById('lossStreakDisplay')
 
-        this.gameSettings ={
-            gamePlay: false,
+        this.gameSettings = {
+            gamePlay: true, //test with true then set to false
             rolls: 0,
             point: 0,
             totalWins: 0,
@@ -57,4 +57,109 @@ class Game {
             win: false
         }
     }
+
+    init(){
+        
+        this.toggleRollBtn(this.gameSettings.gamePlay)
+        this.rollBtn.addEventListener('click', this.rollDice)
+    }
+
+
+
+    // 4
+    checkWin(total, rolls){
+        let result
+        if(total == 2){
+            this.gameSettings.gamePlay = false
+        } else if ((total === 7 || total === 11) && rolls === 1 ){
+            this.gameSettings.win = true
+        } else if ((total != 7 || total != 11) && rolls ===1 ){
+            // settin point
+            this.gameSettings.point = total 
+            this.message.innerText = `Your point is ${this.gameSettings.point}. Lets see if you can hit it.`
+            this.pointDisplay.innerText = this.gameSettings.point
+        } else if (total === 7 && rolls > 1) {
+            this.gameSettings.gamePlay = false
+        } else if (total == this.gameSettings.point && rolls > 1) {
+            this.gameSettings.win = true
+        } else {
+            this.message.innerText = 'Roll again!'
+        }
+
+        this.gameSettings.win ? this.gameSettings.gamePlay = false : null
+
+        if(this.gameSettings.win){
+            this.message.innerText = 'You win'
+            this.message.classList.remove('black')
+            this.message.classList.add('green')
+            result = 'win'
+            this.gameSettings.lastResult = result
+            this.gameSettings.totalWins++
+            this.winDisplay.innerText = this.gameSettings.totalWins
+            this.updateStreak(result)
+            this.toggleRollBtn(this.gameSettings.gamePlay)
+        } else if(this.gameSettings.win == false && this.gameSettings.gamePlay == false){
+            this.message.innerText = 'You Lose'
+            this.message.classList.remove('black')
+            this.message.classList.add('red')
+            result = 'lose'
+            this.gameSettings.lastResult = result
+            this.gameSettings.totalLosses++
+            this.lossDisplay.innerText = this.gameSettings.totalLosses
+            this.updateStreak(result)
+            this.toggleRollBtn(this.gameSettings.gamePlay)
+        }
+    }
+
+    // 3
+    craps(rolls){
+        this.dieDisplay1.innerText = Math.ceil(Math.random() * 6)
+        this.dieDisplay2.innerText = Math.ceil(Math.random() * 6)
+
+        let total 
+        total = parseInt(this.dieDisplay1.innerText) + parseInt(this.dieDisplay2.innerText)
+
+        this.rollTotalDisplay.innerText = total
+        this.rollCountDisplay.innerText = rolls
+
+        this.checkWin(total, rolls)
+    }
+
+
+    //2
+    rollDice(){
+        // because this method is being called when firing an event, we must locate the data from a GLOBAL reference "action"
+        action.gameSettings.rolls++
+        action.craps(action.gameSettings.rolls)
+    }
+
+    //1
+    toggleRollBtn(gamePlay) {
+        if(gamePlay) {
+            this.rollBtn.removeAttribute('disabled')
+        } else {
+            this.rollBtn.setAttribute('disabled', '')
+        }
+
+        // gamePlay ? this.rollBtn.removeAttribute('disabled') : this.rollBtn.setAttribute('disabled', '') // same as the if else chain
+    }
+
+    // 5
+    updateStreak(result){
+        if(result == 'win'){
+            this.gameSettings.lossStreak = 0
+            this.gameSettings.winStreak = 0
+        } else {
+            this.gameSettings.winStreak = 0
+            this.gameSettings.lossStreak++
+        }
+
+        this.winStreakDisplay.innerText = this.gameSettings.winStreak
+        this.lossStreakDisplay.innerText = this.gameSettings.lossStreak
+    }
 }
+
+const gameStartBtn = document.getElementById('gameStartBtn')
+const action = new Game()
+
+gameStartBtn.addEventListener('click',()=> action.init())
